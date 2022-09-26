d8fb9a8d504272870789e635f1f49a89539842cc
Ethan Rooke <ethan@roo.ke>
2022-09-26 01:55:52 -0500

    rough signature parsing

--- grammar.js
+++ grammar.js
@@ -3,9 +3,37 @@ module.exports = grammar({
 
   rules: {
     source_file: ($) =>
-      seq(optional($.module_decl))
+      seq(optional($.module_decl), optional(repeat($.declaration))),
 
     module_decl: ($) => seq("module", $.qual_name),
+
+    declaration: ($) =>
+      choice(
+        $.sig_decl
+        // TODO implement the rest of these
+      ),
+
+    sig_decl: ($) =>
+      seq(
+        optional("var"),
+        optional("abstract"),
+        optional($.mult),
+        "sig",
+        optional(repeat(seq($.name, ","))),
+        $.name,
+        optional($.sig_extension),
+        "{",
+        // TODO: field declarations
+        "}"
+      ),
+
+    sig_extension: ($) =>
+      choice(
+        seq("extends", $.qual_name),
+        seq("in", optional(repeat(seq($.qual_name, "+"))), $.qual_name)
+      ),
+
+    mult: ($) => choice("lone", "some", "one"),
 
     name: ($) => /[a-zA-Z0-9_]+/,
 
